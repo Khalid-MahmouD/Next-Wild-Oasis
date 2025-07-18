755c0d28f5a1ee50aff6dc303a0094d9f19a7f5f
Khalid-MahmouD <40337849+Khalid-MahmouD@users.noreply.github.com>
2025-07-18 17:38:57 +0300

    feat: add Navigation component and Account page

--- app/page.js
+++ app/page.js
@@ -1,11 +1,17 @@
 import Link from "next/link";
+import Navigation from "./components/Navigation";
 
 export default function Home() {
   return (
     <div>
+      <Navigation />
       <h1>The Wild Oasis. Welcome to paradise!</h1>
 
       <Link href="/cabins">Explore Luxurious Cabins</Link>
     </div>
   );
 }
+
+// prefetch all the routes
+// each page will download 
+// each page cached 
