140178570d696f8a37139d4b5814272273fb61d5
Khalid-MahmouD <khaloooood827@gmail.com>
2025-08-19 21:34:24 +0300

    feat: implement TextExpander component and integrate it into cabin detail page

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,3 +1,4 @@
+import TextExpander from "@/app/_components/TextExpander";
 import { getCabin, getCabins } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
@@ -17,7 +18,6 @@ export async function generateMetadata({ params }) {
 }
 
 export async function generateStaticParams() {
-    console.log('xxxxxxxxxxxxxx');
     const cabins = await getCabins();
     const ids = cabins.map(cabin => ({ cabinId: String(cabin.id) }));
     return ids;
@@ -41,7 +41,11 @@ export default async function Page({ params }) {
                         Cabin {name}
                     </h3>
 
-                    <p className="text-lg text-primary-300 mb-10">{description}</p>
+                    <p className="text-lg text-primary-300 mb-10">
+                        <TextExpander>
+                            {description}
+                        </TextExpander>
+                    </p>
 
                     <ul className="flex flex-col gap-4 mb-7">
                         <li className="flex gap-3 items-center">
