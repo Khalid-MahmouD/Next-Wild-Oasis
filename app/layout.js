4a1455507c31e1b2490810b9c2ec0adc60a4c7c6
Khalid-MahmouD <khaloooood827@gmail.com>
2025-08-25 00:43:46 +0300

    feat: implement reservation system with DateSelector, ReservationForm, and context management

--- app/layout.js
+++ app/layout.js
@@ -1,37 +1,38 @@
 
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
 import "@/app/_styles/globals.css";
-
-import { Josefin_Sans } from "next/font/google"; //1
 import Header from "./_components/Header";
-
-const josefin = Josefin_Sans({ //2
-  subsets: ['latin'], // 2.1
-  display: 'swap', // 2.2
-})
+import { ReservationProvider } from "./_components/ReservationContext";
 
 export const metadata = {
   title: {
-    template: "%s | The Wild Oasis",
-    default: "Welcome | The Wild Oasis",
+    template: "%s / The Wild Oasis",
+    default: "Welcome / The Wild Oasis",
   },
-  description: "Luxurious cabins hotel, located in the heart of the Italian Dolomites surrounded by the most beautiful mountains and dark forests.",
-}
-
+  description:
+    "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests",
+};
 
 export default function RootLayout({ children }) {
-  return <html lang='en'>
+  return (
+    <html lang="en">
+      <body
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
+      >
+        <Header />
 
-    <body className={`
-      ${josefin.className} 
-      bg-primary-950 text-primary-100 min-h-screen
-       flex flex-col antialiased relative`}>
-      <Header />
-      <div className="flex-1 px-8 py-12 grid">
-        <main className="max-w-7xl mx-auto w-full">
-          {children}
-        </main>
-      </div>
-
-    </body>
-  </html>
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">
+            <ReservationProvider>{children}</ReservationProvider>
+          </main>
+        </div>
+      </body>
+    </html>
+  );
 }
